d9c9b85f527618701b3bd38f24a213b86cc1672e
Sayak Chatterjee <Sayak Chatterjee>
2019-07-22 13:53:19 +0530

    initial push

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+var path = require('path');
+app.listen(3030);
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+})
